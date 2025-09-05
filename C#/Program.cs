5cb67d0f82f1cc4a7387131b0c0cc15f9f6a4c3e
Sleet <loqueseaidk@gmail.com>
2025-09-05 08:43:02 -0500

    subiendo ejer1 de la semana1

---
+++ C#/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+        static void ejer1()
+        {
+            string nombre, carrera;
+
+            Console.WriteLine("Ingrese su nombre: ");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese su nombre: ");
+            carrera = Console.ReadLine();
+
+            Console.WriteLine($"\n{nombre}, bienvenido al curso de FA de {carrera}");
+        }
+        static void ejer2()
+        {
+            Console.WriteLine("\"Yordan\"");
+        }
+    }
+}
---
+++ Python-code/Python_code.py
@@ -0,0 +1,11 @@
+
+def ejer1():
+    nombre = input("Ingrese su nombre: ")
+    nombre = input("Ingrese su carrera: ")
+
+    print(f"\n{nombre}, bienvenido al FA de {carrera}")
+
+def ejer2():
+    print("\"Yordan\"")
+
+ejer2()
